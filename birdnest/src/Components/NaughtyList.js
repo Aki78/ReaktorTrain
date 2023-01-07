7d61b9157f4d27fc727a418aa8f5b339e389ced5
Aki78 <kmorooka78@gmail.com>
2023-01-07 21:46:24 +0200

    everything working now. next: fancy front end, and setup server

--- birdnest/src/Components/NaughtyList.js
+++ birdnest/src/Components/NaughtyList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import "./NaughtyList.css"
 
 function List() {
+  let count = 0
   const [list, setList] = useState([]);
   const [filteredList, setFilteredList] = useState([]);
 
@@ -9,16 +11,17 @@ function List() {
       const response = await fetch('http://localhost:12345/fetch_recent_naughty_pilots');
       const data = await response.json();
       setList(data);
-      // console.log(data)
+      console.log("data: ", data)
     } catch (error) {
       console.error(error);
     }
+
+
+
   }
 
-  // Call the function every 1.9 seconds
+
   useEffect(() => {
-    const interval = setInterval(fetchList, 1900);
-	  // get unique pilots with the closest distance
     const minValues = list.reduce((acc, curr) => {
       if (!acc[curr.pilot_id] || acc[curr.pilot_id].distance > curr.distance) {
         acc[curr.pilot_id] = curr;
@@ -26,18 +29,36 @@ function List() {
       return acc;
     }, {});
 
-    const new_list = Object.values(minValues);
-	  setFilteredList(new_list)
+    let new_list = Object.values(minValues);
+    console.log("new_list: ", new_list)
+    setFilteredList(new_list)
     console.log("filtededList: ", filteredList)
+    console.log(count+=1)
+
+  }, [list]);
+
+  // Call the function every 1.9 seconds
+  useEffect(() => {
+    const interval = setInterval(fetchList, 1900);
     return () => clearInterval(interval);
 
   }, []);
 
+
   return (
     <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone Number</th>
+          <th>Email</th>
+          <th>Distance</th>
+        </tr>
+      </thead>
       <tbody>
         {filteredList.map(item => (
-          <tr key={item.id} style={{ borderTop: '1px solid black' }}>
+          <tr key={item.email} >
             <td>{item.firstName}</td>
             <td>{item.lastName}</td>
             <td>{item.phoneNumber}</td>
@@ -48,6 +69,7 @@ function List() {
       </tbody>
     </table>
   );
+
 }
 
 export default List;
